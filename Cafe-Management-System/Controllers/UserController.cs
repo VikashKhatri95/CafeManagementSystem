c471734039b110b9f08e952f7454244cf9484837
VikashKhatri95 <Vikashkh2050@gmail.com>
2024-09-25 10:51:30 +0530

    initial Commit

---
+++ Cafe-Management-System/Controllers/UserController.cs
@@ -0,0 +1,164 @@
+﻿using Cafe_Management_System.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Net.Mail; //For Mailing
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Cafe_Management_System.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        CafeEntities db = new CafeEntities();
+        Response response = new Response();
+        [HttpPost,Route("signup")]
+        public HttpResponseMessage SignUp([FromBody] User user)
+        {
+            try {
+                User userobj = db.Users
+                    .Where(u => u.email == user.email).FirstOrDefault();
+                //Check if the user already exists
+                if (userobj == null) {
+                    //If there is no entry i.e if the user is signing up for first time
+                    user.role = "User";
+                    user.status = "false";
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK,new {message="Successfully Registered"});
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest,new {message="Email alreay exists"});
+                }
+            }
+            catch(Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+            
+        }
+        [HttpPost, Route("login")]
+        public HttpResponseMessage Login([FromBody] User user)
+        {
+            try
+            {
+                User userObj=db.Users
+                    .Where(u=>(u.email== user.email && u.password==user.password)).FirstOrDefault();
+                if (userObj != null) {
+                    if (userObj.status == "true")
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, new { token = TokenManager.GenerateToken(userObj.email,userObj.role)});
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Wait for admin approval" });
+                    }
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { message = "Incorrect Username or password" });
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+        [HttpGet,Route("checkToken")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage checkToken()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { message = "true" });
+        }
+        [HttpGet, Route("getAllUser")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage GetAllUser()
+        {
+            try 
+            {
+                var token = Request.Headers.GetValues("authentication").First();
+                TokenClaim tokenClaim=TokenManager.ValidateToken(token);
+                if(tokenClaim.Role!="admin")
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+                var result=db.Users.Select(u=>new {u.id,u.name,u.contactNumber,u.email,u.status,u.role})
+                    .Where(x=>(x.role=="user"))
+                    .ToList();
+                return Request.CreateResponse(HttpStatusCode.OK,result);
+            }
+            catch(Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+        [HttpPost, Route("updateUserStatus")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage UpdateUserStatus(User user)
+        {
+            try {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim=TokenManager.ValidateToken(token);
+                if(tokenClaim.Role != "admin")//Check whether it is admin who is doing the update 
+                {//if no then
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+                //if yes then
+                User userObj = db.Users.Find(user.id);//Check if there is any user with following details
+                if(userObj==null)
+                {//If no such user is present then 
+                    response.message = "User id does not Found";
+                    return Request.CreateResponse(HttpStatusCode.OK,response);
+                }
+                //Change the status 
+                userObj.status = user.status;
+                db.Entry(userObj).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                response.message = "User Status updated Successfully";
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch(Exception e) {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+            
+        }
+        [HttpPost,Route("changePassword")]
+        [CustomAuthenticationFilter]
+        public HttpResponseMessage ChangePassword(ChangePassword changePassword)
+        {
+            try {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim = TokenManager.ValidateToken(token);
+                //Find email and old password
+                User userObj = db.Users
+                    .Where(x => (x.email == tokenClaim.Email && x.password == changePassword.OldPassword)).FirstOrDefault();
+                if (userObj != null)
+                {
+                    userObj.password = changePassword.NewPassword;
+                    db.Entry(userObj).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    response.message = "Password Changed successfully";
+                    return Request.CreateResponse(HttpStatusCode.OK, response);
+                }
+                else
+                {
+                    response.message = "Incorrect Old Password";
+                    return Request.CreateResponse(HttpStatusCode.BadRequest,response);
+                }
+            }
+            catch(Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+
+        }
+    }
+}
---
+++ Cafe-Management-System/TokenManager.cs
@@ -0,0 +1,88 @@
+﻿using Cafe_Management_System.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+namespace Cafe_Management_System
+{
+    public class TokenManager
+    {
+        // Make sure this key is at least 16 bytes long for HMACSHA256
+        public static string Secrect = "randomwordswhichlengthwillbefiftytobesecuredbhbhhhuhoyghi";
+        //Generate Tokens
+        public static string GenerateToken(string email,string role)
+        {
+            // Create a security key from the secret
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secrect));
+            // Define token descriptor
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Email, email), new Claim(ClaimTypes.Role, role) }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            // Create the token
+            JwtSecurityTokenHandler handler =new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+            // Return the serialized token
+            return handler.WriteToken(token);
+        }
+        //
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            try { 
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+                if (jwtToken == null)
+                {
+                    return null;
+                }
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secrect))
+                };
+                SecurityToken securityToken;
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token,parameters,out securityToken);
+                return principal;
+
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static TokenClaim ValidateToken(string RawToken)
+        {
+            string[] array = RawToken.Split(' ');
+            var token = array[1];
+            ClaimsPrincipal principal = GetPrincipal(token);
+            if(principal== null)
+            {
+                return null;
+            }
+            ClaimsIdentity identity = null;
+            try {
+                identity = (ClaimsIdentity)principal.Identity;
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+            TokenClaim tokenClaim = new TokenClaim();
+            var temp = identity.FindFirst(ClaimTypes.Email);
+            tokenClaim.Email = temp.Value;
+            temp=identity.FindFirst(ClaimTypes.Role);
+            tokenClaim.Role= temp.Value;
+            return tokenClaim;
+        }
+    }
+}
